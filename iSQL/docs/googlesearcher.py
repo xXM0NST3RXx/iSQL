c43927b8b11a15fdc3a8b8db96a4dbf64b9164a2
M0NSTƎR <36506755+xXM0NST3RXx@users.noreply.github.com>
2018-02-16 02:27:55 +0300

    Update googlesearcher.py

--- iSQL/docs/googlesearcher.py
+++ iSQL/docs/googlesearcher.py
@@ -1,8 +1,13 @@
 from googlesearch import *
 from docs.cheaker import *
+from urllib.error import URLError, HTTPError
 def googlesrchers(filename,serchword,choice2):
-	for url in search(serchword,stop=150):
-		print(url)
-		with open(filename,mode="a")as typer:
-			typer.write(url+"\n")
+	try:
+		for url in search(serchword,stop=150):
+			print(url)
+			with open(filename,mode="a")as typer:
+				typer.write(url+"\n")
+	except:
+		print("You Brobaly banned from google :/ \n why you don't use broxychains")
+		pass
 		cheaker(url,choice2)
