3eb27474dc4a7801a06b76c9906c4eae13c8a10b
M0NSTƎR <36506755+xXM0NST3RXx@users.noreply.github.com>
2018-02-18 20:14:01 +0300

    Update googlesearcher.py

--- iSQL/docs/googlesearcher.py
+++ iSQL/docs/googlesearcher.py
@@ -1,5 +1,6 @@
 from googlesearch import *
 from docs.cheaker import *
+from termcolor import *
 from urllib.error import URLError, HTTPError
 def googlesrchers(filename,serchword,choice2):
 	try:
@@ -8,5 +9,5 @@ def googlesrchers(filename,serchword,choice2):
 			with open(filename,mode="a")as typer:
 				typer.write(url+"\n")
 	except:
-		print("You Brobaly banned from google :/ \n why you don't use broxychains")
+		print(colored("You Brobaly banned from google :/ \n why you don't use broxychains\n anyway trying again","red"))
 		pass
