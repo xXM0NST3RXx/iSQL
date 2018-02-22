d2e74738329cacc663dea3aee7de6a35744ebb0e
M0NSTƎR <36506755+xXM0NST3RXx@users.noreply.github.com>
2018-02-23 01:51:46 +0300

    Update bing_search.py

--- iSQL/docs/bing_search.py
+++ iSQL/docs/bing_search.py
@@ -7,25 +7,28 @@ def bing(filename,word,choice2):
     pagenumber=11
     sites_list=[]
     while pagenumber <= 561:
-        search={"q":word}
-        seachencode=urllib.parse.urlencode(search)
-        url=Request("http://www.bing.com/search?"+seachencode+"&go=Submit+Query&qs=ds&first="+str(pagenumber)+"&FORM=PERE",headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"})
-        urlopen=req.urlopen(url)
-        html_content=urlopen.read(5000000)
-        soup=BeautifulSoup(html_content,"html.parser")
-        for link in soup.find_all("h2",{"class":""}):
-            try:
-                site=link.a.get("href")
-                if site not in sites_list:
-                    sites_list.append(site)
-                    with open(filename,mode="a")as typer:
-                        typer.write(site+"\n")
-                    print(site)
-                    docs.cheaker.cheaker(site,choice2)
-                else :
+        try:
+            search={"q":word}
+            seachencode=urllib.parse.urlencode(search)
+            url=Request("http://www.bing.com/search?"+seachencode+"&go=Submit+Query&qs=ds&first="+str(pagenumber)+"&FORM=PERE",headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"})
+            urlopen=req.urlopen(url)
+            html_content=urlopen.read(5000000)
+            soup=BeautifulSoup(html_content,"html.parser")
+            for link in soup.find_all("h2",{"class":""}):
+                try:
+                    site=link.a.get("href")
+                    if site not in sites_list:
+                        sites_list.append(site)
+                        with open(filename,mode="a")as typer:
+                            typer.write(site+"\n")
+                        print(site)
+                        docs.cheaker.cheaker(site,choice2)
+                    else :
+                        pass
+                except AttributeError:
                     pass
-            except AttributeError:
-                pass
-        pagenumber=pagenumber+11
-        
+            pagenumber=pagenumber+11
+        except:
+            pass
+            pagenumber=pagenumber+11
     
