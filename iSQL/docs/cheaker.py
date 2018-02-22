86680eaafbc6ab4cb0efbacc8d4b333b8dd8054f
M0NSTƎR <36506755+xXM0NST3RXx@users.noreply.github.com>
2018-02-23 01:52:29 +0300

    Update cheaker.py

--- iSQL/docs/cheaker.py
+++ iSQL/docs/cheaker.py
@@ -3,13 +3,30 @@ from urllib.error import URLError, HTTPError
 from termcolor import *
 import docs.adminfinder
 def cheaker(site,choice2):
+	bad_words = ["http://cxsecurity.com", "https://cxsecurity.com", "https://bing.com", "https://msn.com", "https://microsoft.com",
+				 "https://yahoo.com", "https://live.com", "https://microsofttranslator.com", "https://irongeek.com",
+				 "https://tefneth-import.com", "https://hackforums.net", "https://freelancer.com",
+				 "https://facebook.com", "https://mozilla.org", "https://stackoverflow.com", "https://php.net",
+				 "https://wikipedia.org", "https://amazon.com", "https://4shared.com", "https://wordpress.org",
+				 "https://about.com", "https://phpbuilder.com", "https://phpnuke.org", "https://linearcity.hk",
+				 "https://youtube.com", "https://ptjaviergroup.com", "https://p4kurd.com", "https://tizag.com",
+				 "https://discoverbing.com", "https://devshed.com", "https://ashiyane.org", "https://owasp.org",
+				 "https://1923turk.com", "https://fictionbook.org", "https://silenthacker.do.am", "https://v4-team.com",
+				 "https://codingforums.com", "https://tudosobrehacker.com", "https://zymic.com",
+				 "https://forums.whirlpool.net.au", "https://gaza-hacker.com", "https://immortaltechnique.co.uk",
+				 "https://w3schools.com", "https://phpeasystep.com", "https://mcafee.com",
+				 "https://specialinterestarms.com", "https://pastesite.com", "https://pastebin.com",
+				 "https://joomla.org", "https://joomla.fr", "https://sourceforge.net", "https://joesjewelry.com"]
 	if site.startswith("https")==True: #you can add # before this line and the line after this line (6,7) if you want https results
 		site=""
-	if site[-4:-3] == ".":
+	for bad in bad_words:
+		if site.startswith(bad)==True:
+			site=""
+	if "." in site[-5:]:
 		site=""
 	try:
 		opnurl=req.urlopen(site+"%27")
-		pagescr=str(opnurl.read(100000))
+		pagescr=str(opnurl.read())
 	except HTTPError as err:
 		pagescr=""
 		pass
@@ -32,4 +49,3 @@ def cheaker(site,choice2):
 					print(colored(" [Finding Admin Login] ","yellow").center(60,"-"))
 					docs.adminfinder.adminfind(site)
 				break
-            
